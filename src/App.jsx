030713033050f542e687ff88ed7d11c3d98aa16c
Aqsa Khan <aqsakkhan04@gmail.com>
2026-04-29 20:41:01 +0530

    feat: setup routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
 import LandingPage from './components/landing-page/LandingPage'
 
 function App() {
 
-  return < LandingPage />
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
 }
 
 export default App
